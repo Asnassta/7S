cff2514e1baa2367e26885359779732e7a68a229
Asnassta <79850172+Asnassta@users.noreply.github.com>
2021-10-05 12:49:43 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,10 +77,156 @@ if($(window).width() > 1360) {
 
 	});
 /*=========/smooth scroll=============*/
-
 	
 });
 
+
+/*==================TypedString============*/
+
+let text = ['Психологическая помощь', 'за 2 - 6 недель'];
+
+function typed()
+{
+	
+	
+	if(typeof work !== 'undefined')
+	{
+		return false;
+	}
+	work = 1;
+	
+	//скорость набора
+	let speed = 70;
+	
+	//Через сколько начать печатать новую строку после удаления
+	let timeOutNew = 800;
+
+	//Через сколько начать удалять строку
+	let timeOutDel= 1000;
+	
+	parent = $('#string .typed');
+	
+
+ 
+	string = parent.text();
+	let time = 0;
+	pos = parseInt($(parent).attr('attr-pos'));
+	if(string.length == 0)
+	{
+		str = text[pos].split('');
+		$.each(str, function(i) {
+		  setTimeout( function(){ 
+			  
+			  parent.append(str[i]);
+			  
+			  
+			  if(text[pos].length == parent.text().length)
+			  {
+				  setTimeout( function(){ 
+				  	delete work;
+				  }, timeOutDel);
+			 
+			  }
+			  
+		   }, time)
+		   time += speed;
+		});
+	
+	}
+	else
+	{	
+		str = text[pos].split('');	
+		$.each(str, function(i) {
+		  
+		  setTimeout( function(){ 
+			strd = parent.text();
+			delstr = strd.substring(0, strd.length - 1)
+		  	parent.html(delstr)
+
+		  	if(0 == delstr.length)
+			{
+			  posnew = pos+1;
+			 if((text.length) == posnew)
+			 {
+				 posnew = 0;
+			 }
+		
+			  $(parent).attr('attr-pos', posnew);
+			  setTimeout( function(){ 
+			  	delete work;
+			  }, timeOutNew);
+			  
+			}
+		  	
+		   }, time)
+		   time += speed;
+		    
+		   
+		});
+		
+	}
+	
+	
+}
+
+//задержка перед стартом
+let timeStart = 800;
+setInterval("typed()", timeStart);
+/*==================/TypedString============*/
+
+/*===============Counters==============*/
+//дополнительная функция
+function counterDig(id, i, count)
+{
+	//общее время 
+	var time = 1500;
+	var speed = parseInt(time/count);
+	while(i < count)
+	{ 
+		if(i == 0)
+		{
+			//время первичного запуска
+			time = 100;
+		}
+		setTimeout( function(){ 
+			$('*[attr-id='+id+']').html(parseInt($('*[attr-id='+id+']').text())+1);
+		}, time)
+		
+		i++;
+		time = time+speed ;
+	}
+}
+
+//основная функция
+function DigAnimate()
+{
+	let service=$('#counters span span');
+	$(service).each(function(index) {
+		i = 0;
+		id = $(service[index]).attr('attr-id');
+		count = $(service[index]).attr('attr-end');
+		counterDig(id, i, count);
+	});
+}
+
+
+//запустить при скролле
+run = 0;
+$(window).scroll(function() {
+	parGo = $('.scrollgo');
+   if ($(this).scrollTop() > (parGo.offset().top+parGo.outerHeight()-$(window).height())){
+   if(run == 0)
+	{
+		run = 1;
+		DigAnimate();
+	}
+       
+   }
+});
+/*===============/Counters==============*/
+
+
+/*================WOW - start animation=================*/
 const wow = new WOW({
   boxClass: 'wow', /* класс, который необходим для работы wow.js */
   animateClass: 'animate__animated', /* класс, который будет автоматически добавляться анимируемым элементам при прокрутке страницы */
@@ -89,3 +235,4 @@ const wow = new WOW({
   live: true /* если true, то анимация будет работать даже на динамически добавляемых элементах, если false, то только на тех элементах, которые были на странице при ее загрузке */
 })
 wow.init(); /* Инициализация плагина с установленными выше свойствами */
+/*================WOW - start animation=================*/
